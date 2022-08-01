644fd211b96becc3c15132eafd4902fcc0397bd6
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-08-01 12:05:04 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import AddUser from "./components/Users/AddUser";
+
 
 function App() {
   return (
     <div className="App">
-      test
+      <AddUser/>
     </div>
   );
 }

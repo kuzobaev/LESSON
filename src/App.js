f3e314672f6f1bd85887a83238474204f8097e45
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-08-03 11:54:03 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    console.log('1')
+    const storedUserInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserInformation === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  console.log('2')
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem("isLoggedIn", "true");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false);
   };
 

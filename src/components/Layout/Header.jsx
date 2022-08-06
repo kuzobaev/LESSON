f75bc254762161970a6b85351d8ef40e83b1ed37
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-08-06 12:01:24 +0600

    first looking header layout

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import mealsImage from "../../assets/3007281.jpg";
+import classes from "./Header.module.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button></button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="food" />
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;

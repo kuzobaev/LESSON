728e4b34b035697dcbd6ffed1ebb2f21fbec2ee8
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-08-10 11:13:29 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
+
+import './App.css';
 
 function App() {
-  return <div>MASTER
-    
-  </div>
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
+  );
 }
 
 export default App;

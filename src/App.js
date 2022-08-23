b91f5729304f6c00a5842c17e4938cd8987cdae0
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-08-23 11:30:37 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  return <div>
-    test 
-  </div>
+  const [tasks, setTasks] = useState([]);
+
+  
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/hooks/use-hooks.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async (requestConfig, aplyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.message : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: JSON.stringify(requestConfig.body)
+          ? JSON.stringify(requestConfig.body)
+          : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+ 
+      aplyData(data)
+      // const loadedTasks = [];
+
+      // for (const taskKey in data) {
+      //   loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      // }
+
+      // setTasks(loadedTasks);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+export default useFetch;

086e0cb6c11a647a66d7e5a1a87b288a960766ab
Kamchybek <mac@MacBook-Pro-Mac.local>
2022-09-01 12:06:50 +0600

    save

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import fetchUsers from "./redux/asyncActions/asyncActions";
 
 function App() {
-  const dispatch = useDispatch();
-  const users = useSelector((state) => {
-    return state;
-  });
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          dispatch(fetchUsers());
-        }}
-      >
-        GET USERS
-      </button>
-    </div>
-  );
+  return <div>master</div>;
 }
 
 export default App;
--- src/redux/asyncActions/actions.js
+++
@@ -1,5 +0,0 @@
-import { GET_USERS } from "../reducer/reducer";
-
-export const getUserAction = (payload) => {
-  return { type: GET_USERS, payload };
-};
